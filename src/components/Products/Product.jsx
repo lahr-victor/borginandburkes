931fab67f3ddb450354815913a16c2093a8c1e23
SolForte <mateusbrandao1234@gmail.com>
2023-04-28 18:04:30 -0300

    fix: removal of unnecessary imports

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import { IoMdCart } from 'react-icons/io';
-import ShoppingCart from '../ShoppingCart/ShoppingCart';
 
 export default function Product({
   productId,
